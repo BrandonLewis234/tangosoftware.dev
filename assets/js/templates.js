41ec03753e48cd1ada36fea2f7f8689230b08115
BrandonLewis234 <234brandonlewis@gmail.com>
2026-07-29 23:16:51 -0400

    chore: moved dropdown to settings menu to prepare for projects page

--- assets/js/templates.js
+++ assets/js/templates.js
@@ -29,7 +29,7 @@ class templateSidebar extends HTMLElement
                             <i class="fa fa-angle-double-left"></i>
                             <span>Collapse</span>
                         </a>
-                    </li>                
+                    </li>
                     <li>
                         <a href="/" id="home">
                             <i class="fa fa-home"></i>
@@ -41,7 +41,7 @@ class templateSidebar extends HTMLElement
                             <i class="fa fa-magnifying-glass"></i>
                             <span>Preview</span>
                         </a>
-                    </li>                
+                    </li>
                     <li>
                         <a href="#" id="documentation">
                             <i class="fa fa-book"></i>
@@ -49,25 +49,25 @@ class templateSidebar extends HTMLElement
                         </a>
                     </li>
                     <li>
-                        <a onclick="toggleSubMenu(this)" href="#" id="projects">
+                        <a href="#" id="Projects">
                             <i class="fa fa-lightbulb"></i>
                             <span>Projects</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a onclick="toggleSubMenu(this)" href="#" id="settings">
+                            <i class="fa fa-cog"></i>
+                            <span>Settings</span>
                             <i class="fa fa-angle-right"></i>
                         </a>
                         <ul class="sub-menu">
                             <div>
-                                <li><a href="#">Project 1</a></li>
-                                <li><a href="#">Project 2</a></li>
-                                <li><a href="#">Project 3</a></li>                    
+                                <li><a href="#">nothing</a></li>
+                                <li><a href="#">here</a></li>
+                                <li><a href="#">yet</a></li>                    
                             </div>
                         </ul>
-                    </li>                 
-                    <li>
-                        <a href="#" id="settings">
-                            <i class="fa fa-cog"></i>
-                            <span>Settings</span>
-                        </a>
-                    </li>      
+                    </li>
                     <li>
                         <a onclick="themeToggle()" id="theme-toggle">
                             <i class="fa fa-moon"></i>
