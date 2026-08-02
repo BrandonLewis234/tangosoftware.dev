527daeb51a492611741aa6cf25119eef37077923
BrandonLewis234 <234brandonlewis@gmail.com>
2026-08-01 21:20:58 -0400

    feat: complete Single Page Application implementation using node.js

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const path    = require("path");
 
 const app = express();
 
+app.use("/",       express.static(path.resolve(__dirname)));
 app.use("/static", express.static(path.resolve(__dirname, "frontend", "static")));
+app.use("/images", express.static(path.resolve(__dirname, "frontend", "images")));
 
 app.use((request, response) => 
 {
